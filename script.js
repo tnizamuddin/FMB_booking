7a755566a8d737d8c291412e1a2b77d6602ce1ec
tnizamuddin <150876997+tnizamuddin@users.noreply.github.com>
2023-11-15 13:35:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
-let currentEventId;
+function openBooking(eventId) {
+    var bookingContainer = document.getElementById("booking-" + eventId);
+    var name = prompt("Enter your Name:");
+    var itsNumber = prompt("Enter your ITS Number:");
 
-function openPopup(eventId) {
-    currentEventId = eventId;
-    document.getElementById('overlay').style.display = 'block';
-    document.getElementById('popup').style.display = 'block';
-}
-
-function closePopup() {
-    document.getElementById('overlay').style.display = 'none';
-    document.getElementById('popup').style.display = 'none';
-}
-
-function confirmBooking() {
-    const name = document.getElementById('name').value;
-    const phone = document.getElementById('phone').value;
-
-    // For simplicity, just removing the event from the list
-    const eventElement = document.querySelector(`[data-id="${currentEventId}"]`);
-    eventElement.remove();
-
-    // Display the booked event on the main page
-    displayBookingDetails(currentEventId, name, phone);
-
-    closePopup();
-}
-
-function displayBookingDetails(eventId, name, phone) {
-    const detailsContainer = document.getElementById('eventDetails');
-    const eventDiv = document.createElement('div');
-    eventDiv.innerHTML = `<p>Event ${eventId}: ${name}, ${phone}</p>`;
-    detailsContainer.appendChild(eventDiv);
-}
-
-function authenticate() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Simple authentication logic (replace with secure authentication)
-    if (username === 'admin' && password === 'password') {
-        document.getElementById('loginForm').style.display = 'none';
-        document.getElementById('eventListContainer').style.display = 'block';
-    } else {
-        alert('Invalid credentials. Try again.');
+    if (name && itsNumber) {
+        bookingContainer.innerHTML = "Name: " + name + "<br>ITS Number: " + itsNumber;
+        bookingContainer.style.display = "block";
+        var bookButton = document.getElementById(eventId).querySelector("button");
+        var loginContainer = document.getElementById("login-container");
+        bookButton.style.display = "none";
+        loginContainer.style.display = "block";
     }
+}
+
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Perform authentication here (this is a basic example)
+    if (username === "admin" && password === "password") {
+        showHiddenElements();
+    } else {
+        alert("Invalid credentials");
+    }
+}
+
+function showHiddenElements() {
+    var hiddenElements = document.querySelectorAll(".booking-container");
+    hiddenElements.forEach(function (element) {
+        element.style.display = "block";
+    });
+
+    var bookButtons = document.querySelectorAll(".event-container button");
+    bookButtons.forEach(function (button) {
+        button.style.display = "block";
+    });
+
+    var loginContainer = document.getElementById("login-container");
+    loginContainer.style.display = "none";
 }

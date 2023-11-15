888f16efe7eeedc8a377733b5c5a1c0d4258c1b1
tnizamuddin <150876997+tnizamuddin@users.noreply.github.com>
2023-11-15 11:59:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const eventList = document.getElementById('eventList');
 
-    // Generate 50 sample events
-    const events = Array.from({ length: 50 }, (_, index) => `Event ${index + 1}`);
+    // Sample event data
+    const events = ['Event 1', 'Event 2', 'Event 3', 'Event 4', 'Event 5'];
 
     // Populate the event list
     events.forEach(event => {
@@ -14,9 +14,50 @@ document.addEventListener('DOMContentLoaded', function () {
         bookButton.addEventListener('click', function () {
             // Remove the booked event from the list
             eventList.removeChild(listItem);
+
+            // Show the name input field
+            const nameInput = document.getElementById('nameInput');
+            const bookBtn = document.getElementById('bookBtn');
+            nameInput.style.display = 'block';
+            bookBtn.dataset.event = event; // Save the event name in the button dataset
         });
 
         listItem.appendChild(bookButton);
         eventList.appendChild(listItem);
     });
+
+    // Add event listener for the 'Book' button after it's dynamically created
+    document.getElementById('bookBtn').addEventListener('click', function () {
+        // Get the entered name
+        const name = document.getElementById('name').value;
+
+        // Get the booked event from the button dataset
+        const bookedEvent = this.dataset.event;
+
+        // Send email notification
+        sendEmailNotification(name, bookedEvent);
+
+        // Hide the name input field
+        const nameInput = document.getElementById('nameInput');
+        nameInput.style.display = 'none';
+    });
+
+    function sendEmailNotification(name, bookedEvent) {
+        // Replace these placeholders with your Email.js Service ID, User ID, and Template ID
+        const emailjsParams = {
+            serviceId: 'your_service_id',
+            userId: 'your_user_id',
+            templateId: 'your_template_id',
+        };
+
+        emailjs.send(emailjsParams.serviceId, emailjsParams.templateId, {
+            eventName: bookedEvent,
+            attendeeName: name,
+        }, emailjsParams.userId)
+            .then(function (response) {
+                console.log('Email sent successfully:', response);
+            }, function (error) {
+                console.error('Email send failed:', error);
+            });
+    }
 });
